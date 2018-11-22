0222704c84b6970b6dc68ba35d7553e98c7b0b77
tremoctopus <tremoctopus@rambler.ru>
2018-11-22 21:16:51 +0300

    added methods for RandomIntAnnotationProcessor
    isFieldTypeInteger
    setFieldValue - throws AnnotationInvalidFieldTypeException

--- lesson2/src/main/java/RandomIntAnnotationProcessor.java
+++ lesson2/src/main/java/RandomIntAnnotationProcessor.java
@@ -1,28 +1,39 @@
+import exceptions.AnnotationInvalidFieldTypeException;
+
 import java.lang.reflect.Field;
 import java.util.Random;
 
 public class RandomIntAnnotationProcessor {
     public static void setField(Object object) throws IllegalAccessException {
-        // 1. Получаем объекта класса Class
-        // 2. Находим все его поля
-        // 3. Находим аннтоции RandomInt
-        // 4. Устанавливаем значение
         Class<?> objectClass = object.getClass();
         Field[] fields = objectClass.getDeclaredFields();
 
         for (Field field: fields) {
-            System.out.println("Field: " + field.getName());
-            System.out.println("Field type: " + field.getType());
             RandomInt annotation = field.getAnnotation(RandomInt.class);
-            System.out.println("Is null: " + (annotation == null));
             if (annotation != null) {
                 int number = new Random()
                         .nextInt(annotation.max() - annotation.min() + 1) + annotation.min();
-                System.out.println("Number: " + number);
-                field.setAccessible(true);
-                field.set(object, number);
+                try {
+                    setFieldValue(field, object, number);
+                } catch (AnnotationInvalidFieldTypeException e) {
+                    e.printStackTrace();
+                }
             }
         }
+    }
 
+    private static void setFieldValue(Field field, Object o, int value) throws IllegalAccessException, AnnotationInvalidFieldTypeException {
+
+        String typeName = field.getType().getSimpleName();
+        if(isFieldTypeInteger(typeName)){
+            field.setAccessible(true);
+            field.set(o, value);
+        }else {
+            throw new AnnotationInvalidFieldTypeException("Field type is not valid. Required: Integer, found: " + typeName);
+        }
+    }
+
+    private static boolean isFieldTypeInteger(String typeName){
+        return typeName.equals("int") || typeName.equals("Integer");
     }
 }
