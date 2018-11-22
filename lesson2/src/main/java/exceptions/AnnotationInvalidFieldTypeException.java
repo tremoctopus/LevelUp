9eedd3c056def33ae2fcdb07a58477685a8963f9
tremoctopus <tremoctopus@rambler.ru>
2018-11-22 20:43:33 +0300

    new exception
    added new AnnotationInvalidFieldTypeException
    to be thrown from RandomIntAnnotationProcessor

---
+++ lesson2/src/main/java/exceptions/AnnotationInvalidFieldTypeException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class AnnotationInvalidFieldTypeException extends Exception {
+    public AnnotationInvalidFieldTypeException(String message) {
+        super(message);
+    }
+}
