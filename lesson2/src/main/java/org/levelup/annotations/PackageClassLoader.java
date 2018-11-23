7ad7c76bf978f15fea5a3bab0717b72766ccab0d
tremoctopus <tremoctopus@rambler.ru>
2018-11-23 21:48:39 +0300

    added class
    added empty class PackageClassLoader

---
+++ lesson2/src/main/java/org/levelup/annotations/PackageClassLoader.java
@@ -0,0 +1,10 @@
+package org.levelup.annotations;
+
+import java.util.List;
+
+public class PackageClassLoader extends ClassLoader {
+
+    public List<Object> getObjects(String packageName){
+        return null;
+    }
+}
